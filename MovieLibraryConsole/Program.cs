6b638c951b91bdf1379ebd83ffd336b10615be1a
Manders93 <asedgwick2@my.wctc.edu>
2024-05-19 12:32:25 -0500

    done!

--- MovieLibraryConsole/Program.cs
+++ MovieLibraryConsole/Program.cs
@@ -25,115 +25,393 @@ namespace MovieApp
             _logger.LogInformation($"Added new movie: {title}");
         }
 
-        public void EditMovie(MovieContext db)
+        public void AddUser(MovieContext db)
         {
-            Console.WriteLine("Enter the title of the movie to edit:");
-            var title = Console.ReadLine();
-            var movie = db.Movies.FirstOrDefault(m => m.Title == title);
-            if (movie != null)
+            Console.WriteLine("Enter user details:");
+            Console.WriteLine("Name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Age:");
+            var age = int.Parse(Console.ReadLine()); // You should add error handling for invalid inputs
+            Console.WriteLine("Occupation:");
+            var occupationName = Console.ReadLine();
+
+            // Check if the occupation already exists
+            var occupation = db.Occupations.FirstOrDefault(o => o.Name == occupationName);
+            if (occupation == null)
             {
-                Console.WriteLine("Enter the new title for the movie:");
-                var newTitle = Console.ReadLine();
-                movie.Title = newTitle;
+                // If the occupation doesn't exist, create a new one
+                occupation = new Occupation { Name = occupationName };
+                db.Occupations.Add(occupation);
                 db.SaveChanges();
-                _logger.LogInformation($"Edited movie '{title}' to '{newTitle}'");
             }
-            else
-            {
-                Console.WriteLine($"Movie '{title}' not found.");
-            }
-        }
 
-        public void DisplayMovies(MovieContext db)
-        {
-            var movies = db.Movies.ToList();
-            Console.WriteLine("All movies:");
-            //////foreach (var movie in movies)
-            //////{
-            //////    Console.WriteLine($"Title: {movie.Title}");
-            //////    // Display genres alongside movie title
-            //////    Console.WriteLine("Genres:");
-            //////    var genre = " ";
-            //////    foreach (var genre in movie.MovieGenres.Select(mg => mg.Genre.Name))
-            //////    {
-            //////        Console.WriteLine($"\t{genre}");
-            //////    }
-            //////    Console.WriteLine();
-            //////}
-            _logger.LogInformation($"Displayed all {movies.Count} movies.");
-        }
+            // Create a new user and associate it with the occupation
 
-        public void SearchMovie(MovieContext db)
-        {
-            Console.WriteLine("Enter the title of the movie to search:");
-            var title = Console.ReadLine();
-            var movie = db.Movies.FirstOrDefault(m => m.Title.Contains(title));
-            if (movie != null)
+
+            var newUser = new User
             {
-                Console.WriteLine($"Title: {movie.Title}");
-                // Display genres alongside movie title
-                Console.WriteLine("Genres:");
-                foreach (var genre in movie.MovieGenres.Select(mg => mg.Genre.Name))
+                Name = name,
+                Age = age,
+                OccupationId = (int)occupation.Id
+            };
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            _logger.LogInformation($"Added new user: {name}");
+
+            //    var newUser = new User
+            //    {
+            //        Name = name,
+            //        Age = age,
+            //        OccupationId = (int)occupation.Id
+            //    };
+            //    db.Users.Add(newUser);
+            //    db.SaveChanges();
+
+            //    _logger.LogInformation($"Added new user: {name}");
+            //}
+        }
+            public void EditMovie(MovieContext db)
+            {
+                Console.WriteLine("Enter the title of the movie to edit:");
+                var title = Console.ReadLine();
+                var movie = db.Movies.FirstOrDefault(m => m.Title == title);
+                if (movie != null)
                 {
-                    Console.WriteLine($"\t{genre}");
+                    Console.WriteLine("Enter the new title for the movie:");
+                    var newTitle = Console.ReadLine();
+                    movie.Title = newTitle;
+                    db.SaveChanges();
+                    _logger.LogInformation($"Edited movie '{title}' to '{newTitle}'");
+                }
+                else
+                {
+                    Console.WriteLine($"Movie '{title}' not found.");
                 }
             }
-            else
-            {
-                Console.WriteLine($"Movie '{title}' not found.");
-            }
-            _logger.LogInformation($"Searched for movie: {title}");
-        }
 
-        public void Run()
-        {
-            using (var db = new MovieContext())
+            public void DisplayMovies(MovieContext db)
             {
-                while (true)
+                var movies = db.Movies.ToList();
+                Console.WriteLine("All movies:");
+                foreach (var movie in movies)
                 {
-                    Console.WriteLine("Choose an option:");
-                    Console.WriteLine("1. Add Movie");
-                    Console.WriteLine("2. Edit Movie");
-                    Console.WriteLine("3. Display All Movies");
-                    Console.WriteLine("4. Search Movie");
-                    Console.WriteLine("5. Exit");
+                    Console.WriteLine($"Title: {movie.Title}");
+                    // Display genres alongside movie title
+                    Console.WriteLine("Genres:");
 
-                    var choice = Console.ReadLine();
-
-                    switch (choice)
+                    if (movie.MovieGenres != null && movie.MovieGenres.Any())
                     {
-                        case "1":
-                            AddMovie(db);
-                            break;
-                        case "2":
-                            EditMovie(db);
-                            break;
-                        case "3":
-                            DisplayMovies(db);
-                            break;
-                        case "4":
-                            SearchMovie(db);
-                            break;
-                        case "5":
-                            return;
-                        default:
-                            Console.WriteLine("Invalid choice. Please try again.");
-                            break;
+                        foreach (var movieGenre in movie.MovieGenres.Select(mg => mg.Genre.Name))
+                        {
+                            Console.WriteLine($"\t{movieGenre}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\tNo genres found.");
+                    }
+                    Console.WriteLine();
+                }
+                _logger.LogInformation($"Displayed all {movies.Count} movies.");
+            }
+
+            public void SearchMovie(MovieContext db)
+            {
+                Console.WriteLine("Enter the title of the movie to search:");
+                var title = Console.ReadLine();
+                var movie = db.Movies.FirstOrDefault(m => m.Title.Contains(title));
+                if (movie != null)
+                {
+                    Console.WriteLine($"Title: {movie.Title}");
+                    // Display genres alongside movie title
+                    Console.WriteLine("Genres:");
+                    if (movie.MovieGenres != null)
+                    {
+                        foreach (var genre in movie.MovieGenres.Select(mg => mg.Genre.Name))
+                        {
+                            Console.WriteLine($"\t{genre}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\tNo genres found.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Movie '{title}' not found.");
+                }
+                _logger.LogInformation($"Searched for movie: {title}");
+            }
+
+            public void Run()
+            {
+                using (var db = new MovieContext())
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Choose an option:");
+                        Console.WriteLine("1. Add Movie");
+                        Console.WriteLine("2. Edit Movie");
+                        Console.WriteLine("3. Display All Movies");
+                        Console.WriteLine("4. Search Movie");
+                        Console.WriteLine("5. Add User");
+                        Console.WriteLine("6. Rate Movie");
+                        Console.WriteLine("7. Display User Rated Movies");
+                        Console.WriteLine("8. Exit");
+
+                        var choice = Console.ReadLine();
+
+                        switch (choice)
+                        {
+                            case "1":
+                                AddMovie(db);
+                                break;
+                            case "2":
+                                EditMovie(db);
+                                break;
+                            case "3":
+                                DisplayMovies(db);
+                                break;
+                            case "4":
+                                SearchMovie(db);
+                                break;
+                            case "5":
+                                AddUser(db);
+                                break;
+                            case "6":
+                                // Implement RateMovie(db);
+                                Console.WriteLine("Rate Movie option is not implemented yet.");
+                                break;
+                            case "7":
+                                // Implement DisplayUserRatedMovies(db);
+                                Console.WriteLine("Display User Rated Movies option is not implemented yet.");
+                                break;
+                            case "8":
+                                return;
+                            default:
+                                Console.WriteLine("Invalid choice. Please try again.");
+                                break;
+                        }
                     }
                 }
             }
-        }
 
-        private static void Main(string[] args)
-        {
-            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-            var logger = loggerFactory.CreateLogger<Program>();
-            var program = new Program(logger);
-            program.Run();
+            private static void Main(string[] args)
+            {
+                var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+                var logger = loggerFactory.CreateLogger<Program>();
+                var program = new Program(logger);
+                program.Run();
+            }
         }
     }
-}
 
+
+
+
+
+
+
+
+
+
+//using System;
+//using System.Linq;
+//using Microsoft.Extensions.Logging;
+//using MovieLibraryEntities.Context;
+//using MovieLibraryEntities.Models;
+
+//namespace MovieApp
+//{
+//    internal class Program
+//    {
+//        private readonly ILogger<Program> _logger;
+
+//        public Program(ILogger<Program> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public void AddMovie(MovieContext db)
+//        {
+//            Console.WriteLine("Enter the title of the movie to add:");
+//            var title = Console.ReadLine();
+//            var newMovie = new Movie { Title = title };
+//            db.Movies.Add(newMovie);
+//            db.SaveChanges();
+//            _logger.LogInformation($"Added new movie: {title}");
+//        }
+//        public void AddUser(MovieContext db)
+//        {
+//            Console.WriteLine("Enter user details:");
+//            Console.WriteLine("Name:");
+//            var name = Console.ReadLine();
+//            Console.WriteLine("Age:");
+//            var age = int.Parse(Console.ReadLine()); // You should add error handling for invalid inputs
+//            Console.WriteLine("Occupation:");
+//            var occupationName = Console.ReadLine();
+
+//            // Check if the occupation already exists
+//            var occupation = db.Occupations.FirstOrDefault(o => o.Name == occupationName);
+//            if (occupation == null)
+//            {
+//                // If the occupation doesn't exist, create a new one
+//                occupation = new Occupation { Name = occupationName };
+//                db.Occupations.Add(occupation);
+//                db.SaveChanges();
+//            }
+
+//            // Create a new user and associate it with the occupation
+//            var newUser = new User
+//            {
+//                Name = name,
+//                Age = age,
+//                OccupationId = occupation.Id
+//            };
+//            db.Users.Add(newUser);
+//            db.SaveChanges();
+
+//            _logger.LogInformation($"Added new user: {name}");
+//        }
+
+//        public void EditMovie(MovieContext db)
+//        {
+//            Console.WriteLine("Enter the title of the movie to edit:");
+//            var title = Console.ReadLine();
+//            var movie = db.Movies.FirstOrDefault(m => m.Title == title);
+//            if (movie != null)
+//            {
+//                Console.WriteLine("Enter the new title for the movie:");
+//                var newTitle = Console.ReadLine();
+//                movie.Title = newTitle;
+//                db.SaveChanges();
+//                _logger.LogInformation($"Edited movie '{title}' to '{newTitle}'");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Movie '{title}' not found.");
+//            }
+//        }
+
+//        public void DisplayMovies(MovieContext db)
+//        {
+//            var movies = db.Movies.ToList();
+//            Console.WriteLine("All movies:");
+//            foreach (var movie in movies)
+//            {
+//                Console.WriteLine($"Title: {movie.Title}");
+//                // Display genres alongside movie title
+//                Console.WriteLine("Genres:");
+
+//                if (movie.MovieGenres != null && movie.MovieGenres.Any())
+//                {
+//                    foreach (var movieGenre in movie.MovieGenres.Select(mg => mg.Genre.Name))
+//                    {
+//                        Console.WriteLine($"\t{movieGenre}");
+//                    }
+//                }
+//                else
+//                {
+//                    Console.WriteLine("\tNo genres found.");
+//                }
+//                Console.WriteLine();
+//            }
+//            _logger.LogInformation($"Displayed all {movies.Count} movies.");
+//        }
+
+//        public void SearchMovie(MovieContext db)
+//        {
+//            Console.WriteLine("Enter the title of the movie to search:");
+//            var title = Console.ReadLine();
+//            var movie = db.Movies.FirstOrDefault(m => m.Title.Contains(title));
+//            if (movie != null)
+//            {
+//                Console.WriteLine($"Title: {movie.Title}");
+//                // Display genres alongside movie title
+//                Console.WriteLine("Genres:");
+//                if (movie.MovieGenres != null)
+//                {
+//                    foreach (var genre in movie.MovieGenres.Select(mg => mg.Genre.Name))
+//                    {
+//                        Console.WriteLine($"\t{genre}");
+//                    }
+//                }
+//                else
+//                {
+//                    Console.WriteLine("\tNo genres found.");
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Movie '{title}' not found.");
+//            }
+//            _logger.LogInformation($"Searched for movie: {title}");
+//        }
+
+//        public void Run()
+//        {
+//            using (var db = new MovieContext())
+//            {
+//                while (true)
+//                {
+//                    Console.WriteLine("Choose an option:");
+//                    Console.WriteLine("1. Add Movie");
+//                    Console.WriteLine("2. Edit Movie");
+//                    Console.WriteLine("3. Display All Movies");
+//                    Console.WriteLine("4. Search Movie");
+//                    Console.WriteLine("5. Add User");
+//                    Console.WriteLine("6. Rate Movie");
+//                    Console.WriteLine("7. Display User Rated Movies");
+//                    Console.WriteLine("8. Exit");
+
+//                    var choice = Console.ReadLine();
+
+//                    switch (choice)
+//                    {
+//                        case "1":
+//                            AddMovie(db);
+//                            break;
+//                        case "2":
+//                            EditMovie(db);
+//                            break;
+//                        case "3":
+//                            DisplayMovies(db);
+//                            break;
+//                        case "4":
+//                            SearchMovie(db);
+//                            break;
+//                        case "5":
+//                            AddUser(db);
+//                            break;
+//                        case "6":
+//                            RateMovie(db);
+//                            break;
+//                        case "7":
+//                            DisplayUserRatedMovies(db);
+//                            break;
+//                        case "8":
+//                            return;
+//                        default:
+//                            Console.WriteLine("Invalid choice. Please try again.");
+//                            break;
+//                    }
+//                }
+//            }
+//        }
+
+
+
+//        private static void Main(string[] args)
+//        {
+//            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+//            var logger = loggerFactory.CreateLogger<Program>();
+//            var program = new Program(logger);
+//            program.Run();
+//        }
+//    }
+//}
 
 
 
--- MovieLibraryEntities/Models/User.cs
+++ MovieLibraryEntities/Models/User.cs
@@ -2,11 +2,16 @@
 
 public class User
 {
-    public long Age { get; set; }
-    public string Gender { get; set; }
-    public long Id { get; set; }
-
-    public virtual Occupation Occupation { get; set; }
-    public virtual ICollection<UserMovie> UserMovies { get; set; }
-    public string ZipCode { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int OccupationId { get; set; } // Change type to int
+    public Occupation Occupation { get; set; }
 }
+//public long Age { get; set; }
+//public string Gender { get; set; }
+//public long Id { get; set; }
+
+//public virtual Occupation Occupation { get; set; }
+//public virtual ICollection<UserMovie> UserMovies { get; set; }
+//public string ZipCode { get; set; }
